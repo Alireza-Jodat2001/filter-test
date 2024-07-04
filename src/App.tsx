ad10a05339120bde26ea875d5e3a76411264541e
Amirhomayoun Azizi <AlirezaJodat2001@gmail.com>
2024-07-04 14:12:06 +0330

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,59 @@
-import { useDispatch } from "react-redux";
-import { getData } from "./hook/productsSlice";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function App() {
-   const productsDispatch = useDispatch();
+   const [allData, setAllData] = useState([]);
+   const [allCategories, setAllCategories] = useState([]);
+   const [chooseProducts, setChooseProducts] = useState([]);
+
+   // const [currCat, setCurrCat] = useState([]);
+   const [maxPrice, setMaxPrice] = useState<number>();
+   const [priceFiltered, setPriceFiltered] = useState<number>();
+
+   async function getData(route) {
+      try {
+         const { data } = await axios.get(`https://fakestoreapi.com${route}`);
+         return data;
+      } catch (err) {
+         console.log(err);
+      }
+   }
+
+   useEffect(() => {
+      (async (route) => {
+         const data = await getData(route);
+         const categories = Array.from(
+            new Set(data.map(({ category }) => category))
+         );
+
+         setAllCategories(categories);
+         setAllData(data);
+      })("/products");
+   }, []);
+
+   function showProducts(category) {
+      // filter category
+      const chooseCat = allData.filter(
+         (product) => product.category === category
+      );
+      // let maxPrice: number;
+      // currCat.forEach(({ price }, index) => {
+      //    if (index === 0) maxPrice = price;
+      //    if (price > maxPrice) maxPrice = price;
+      // });
+      // setMaxPrice(maxPrice);
+      setChooseProducts(chooseCat);
+   }
+
+   function changeMaxPrice() {
+      // const currPrice = allData.filter(
+      //    (product) => product.price <= priceFiltered
+      // );
+      // setCurrCat(currPrice);
+   }
 
    return (
-      <div className="h-full bg-slate-500 p-[20px] flex gap-[15px]">
+      <div className="min-h-full bg-slate-500 p-[20px] flex gap-[15px]">
          <div className="[&>*]:w-[90%] w-[65%] [&>*]:mx-auto">
             <form className="bg-white h-[40px] rounded-sm font-[500] flex gap-[10px] items-center mb-[30px]">
                <button
@@ -13,7 +61,6 @@ export default function App() {
                   type="submit"
                   onClick={(e) => {
                      e.preventDefault();
-                     productsDispatch(getData());
                   }}
                >
                   Search
@@ -26,17 +73,60 @@ export default function App() {
                />
             </form>
 
-            <div>
-               <div className="w-[90%] mx-auto">
-                  <img src="" alt="" className="w-[95%] aspect-[4/3] m-auto" />
-                  <h4></h4>
-                  <p></p>
-                  <p></p>
-                  <p></p>
-               </div>
+            <div className="flex flex-wrap gap-[10px]">
+               {allCategories.map((category, index) => {
+                  return (
+                     <button
+                        key={index}
+                        className="text-black bg-white px-[10px] rounded-sm font-[500]"
+                        onClick={() => showProducts(category)}
+                     >
+                        {category}
+                     </button>
+                  );
+               })}
+            </div>
+
+            <div className="mt-[50px] grid gap-[30px]">
+               {chooseProducts?.map(
+                  ({ title, image, description, price }, index) => (
+                     <div
+                        key={index}
+                        className="w-[90%] mx-auto p-[30px] grid gap-[30px] bg-white rounded-lg"
+                     >
+                        <img
+                           src={image}
+                           alt={title}
+                           className="h-[200px] aspect-auto m-auto "
+                        />
+                        <h4>{title}</h4>
+                        <p className="">{description}</p>
+                        <p>{price} $</p>
+                     </div>
+                  )
+               )}
             </div>
          </div>
-         <aside className="bg-white w-[35%] rounded-sm"></aside>
+         <aside className="bg-white w-[35%] rounded-sm">
+            {chooseProducts.length && (
+               <>
+                  <div className="flex w-full justify-evenly mb-[15px]">
+                     <span>0 $</span>
+                     <input
+                        type="range"
+                        min={0}
+                        max={maxPrice}
+                        onChange={(e) => {
+                           setPriceFiltered(e.target.value);
+                           changeMaxPrice();
+                        }}
+                     />
+                     <span>{maxPrice} $</span>
+                  </div>
+                  <div>To: {priceFiltered}</div>
+               </>
+            )}
+         </aside>
       </div>
    );
 }
